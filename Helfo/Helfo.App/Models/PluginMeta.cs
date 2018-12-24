e2e6b990a882d716e467fd42ca7aa781edc88502
Müller Mathias <illinariss@gmx.de>
2018-12-24 01:45:46 +0100

    Simple Test for getting used to Blazor

---
+++ Helfo/Helfo.App/Models/PluginMeta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helfo.App.Models
+{
+    public class PluginMeta
+    {
+       
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string Version { get; set; }
+        public DateTime InstalledOn { get; set; }
+        public int Priority { get; set; }
+        
+    }
+}
